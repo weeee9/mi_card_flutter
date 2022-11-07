f2186e4ac502a78d632aa8ce450f0e289a303265
周身鴻 <weeee9@MacBookAir.local>
2022-11-07 21:44:51 +0800

    chore: single-child layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(5.0),
+            color: Colors.white,
+            child: Text("Hello"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
