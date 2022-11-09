e6442aa9f735223192c7b40711be537665765d2a
周身鴻 <weeee9@MacBookAir.local>
2022-11-09 20:30:57 +0800

    feat: mi_card

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,83 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.all(10.0),
-            padding: EdgeInsets.all(5.0),
-            color: Colors.white,
-            child: Text("Hello"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 60.0,
+                backgroundColor: Colors.grey[300],
+                backgroundImage: AssetImage("images/finn_avatar.png"),
+              ),
+              Text(
+                "Finn Mertens",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                "Warrior of the Land of Ooo",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal[50],
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(color: Colors.teal[50]),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "+444 5552471",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    "finn-the-human@landofooo.com",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.teal,
+                      fontFamily: 'Source Sans Pro',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
